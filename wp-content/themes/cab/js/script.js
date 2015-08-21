869aba76363c8671be34009664151bb12ff595cd
kellenkm <kellenkmorimoto@gmail.com>
2015-08-20 17:46:53 -0700

    style updates for wp

--- wp-content/themes/cab/js/script.js
+++ wp-content/themes/cab/js/script.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
 	//     updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
 	// });
 	$(window).load(function() {
-		var s = skrollr.init();
+		var s = skrollr.init({
+			smoothScrolling: false
+		});
 	});
 	$('#show-all-exhibitors').click(function() {
 		// $(this).toggle();
